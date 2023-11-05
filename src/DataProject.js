32cee834fc57f105b76595c6e14bf1a37835b0ea
WEB-L00P <105985281+WEB-L00P@users.noreply.github.com>
2023-11-05 19:13:20 +0530

    Add Watch App Project

--- src/DataProject.js
+++ src/DataProject.js
@@ -1,5 +1,11 @@
 export const DataProject = [
   {
+    title: "Watch Movie App",
+    para: "Watch Movie Web Application created using NextJs 14 and TheMovieDB API.",
+    img: "https://media.discordapp.net/attachments/1066265844167999488/1170719342023950376/Home-screen.jpg?ex=655a105a&is=65479b5a&hm=f3c64adf7ac67b9cb4e9d6c55477a8bd0bbb54895ff36b6d65380f61fd5f28dc&=&width=1231&height=876",
+    link: "https://watch-app-three.vercel.app/",
+  },
+  {
     title: "Codedamn Hacktoberfest 2023",
     para: "Solving all 30Days coding challange by codedamn. Track my progress and code by below link",
     img: "https://media.discordapp.net/attachments/1066265844167999488/1160762139481493585/Hacktoberfest-2023-Codedamn.png?ex=6535d6fb&is=652361fb&hm=df7f4a15de7fd28737b594a7e8529839355c9ae213f768d2a316d76526cc2176&=&width=1551&height=888",
